a0cea089451237737faead60707d85fcabe63be8
Deborah Okeke <okekedeborah@gmail.com>
2026-03-26 08:38:14 +0100

    fix (build): disable payment and webhook routes

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -83,17 +83,17 @@ router
       .only(['store', 'index'])
       .middleware('store', [middleware.auth(), middleware.role({ role: UserRolesEnum.Farmer })])
 
-    router
-      .post('payments/callback', [() => import('#controllers/payments_controller'), 'callback'])
-      .as('payments.callback')
-    // .use(middleware.auth())
+    // router
+    //   .post('payments/callback', [() => import('#controllers/payments_controller'), 'callback'])
+    //   .as('payments.callback')
+    // // .use(middleware.auth())
 
-    router
-      .post('webhooks/interswitch', [
-        () => import('#controllers/webhooks_controller'),
-        'interswitch',
-      ])
-      .as('webhooks.interswitch')
+    // router
+    //   .post('webhooks/interswitch', [
+    //     () => import('#controllers/webhooks_controller'),
+    //     'interswitch',
+    //   ])
+    //   .as('webhooks.interswitch')
   })
   .prefix('api/v1')
   .as('api.v1')
