a1cb39ec8107b43cff5d20af61cf17063e88d3ea
Deborah Okeke <okekedeborah@gmail.com>
2026-04-21 09:54:25 +0100

    fix: switch from dynamic imports to static imports in the `routes.ts` file to ensure that all routes are loaded and available when the app boots

--- start/routes.ts
+++ start/routes.ts
@@ -7,13 +7,13 @@
 |
 */
 
-import('#start/routes/api/v1/user_routes')
-import('#start/routes/api/v1/agro_dealer_profile_routes')
-import('#start/routes/api/v1/farmer_profile_routes')
-import('#start/routes/api/v1/authentication_routes')
-import('#start/routes/api/v1/bank_routes')
-import('#start/routes/api/v1/crop_scan_routes')
-import('#start/routes/api/v1/product_routes')
-import('#start/routes/api/v1/order_routes')
-import('#start/routes/api/v1/payment_routes')
-import('#start/routes/api/v1/webhook_routes')
+import '#start/routes/api/v1/user_routes'
+import '#start/routes/api/v1/agro_dealer_profile_routes'
+import '#start/routes/api/v1/farmer_profile_routes'
+import '#start/routes/api/v1/authentication_routes'
+import '#start/routes/api/v1/bank_routes'
+import '#start/routes/api/v1/crop_scan_routes'
+import '#start/routes/api/v1/product_routes'
+import '#start/routes/api/v1/order_routes'
+import '#start/routes/api/v1/payment_routes'
+import '#start/routes/api/v1/webhook_routes'
